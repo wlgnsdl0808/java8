903111df112b0510e05f8c070205b585d049b84c
wlgnsdl0808 <wlgnsdlwwkd@naver.com>
2022-09-02 17:37:19 +0900

    add java print

--- ch02/src/sec1/Var02.java
+++ ch02/src/sec1/Var02.java
@@ -2,9 +2,15 @@ package sec1;
 
 public class Var02 {
 
+	int a=10, b=20;		//전역 변수 - 현재 클래스 내부에서 어디서든지 인식
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=100, b=200;	//지역 변수 - 현재 메서드 안에서만 인식
+		System.out.println("a : "+a);
+		tot(a,b); //메서드 호출
 	}
 
+	public static void tot(int f, int g){	//매개변수 - 메서드 호출시 인식
+		System.out.println("a+b="+(f+g));
+		
+	}
 }
---
+++ ch02/src/sec3/Print1.java
@@ -0,0 +1,17 @@
+package sec3;
+
+public class Print1 {
+
+	public static void main(String[] args) {
+		System.out.print("a\n");
+		System.out.print("b\n");
+		System.out.print("c\n");
+		
+		System.out.println("d");
+		System.out.println("e");
+		System.out.println("f");
+		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\\\"");
+
+	}
+
+}
