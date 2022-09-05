30fa7bceeac309164f00b5173ea76ca27dacf30b
wlgnsdl0808 <wlgnsdlwwkd@naver.com>
2022-09-05 18:02:31 +0900

    add java operation1

---
+++ ch03/src/sec1/Oper2.java
@@ -0,0 +1,18 @@
+package sec1;
+
+public class Oper2 {
+
+	public static void main(String[] args) {
+		int a=15, b=20;
+		System.out.println("a+b="+(a+b));	//더하기
+		System.out.println("a-b="+(a-b));	//빼기
+		System.out.println("b*a="+(b*a));	//곱하기
+		System.out.println("b/a="+((float)b/a)); //나누기
+		System.out.println("b%a="+(b%a));	//나머지
+		//부호 연산자
+		System.out.println(-a);
+		System.out.println(-(a-b));
+
+	}
+
+}
